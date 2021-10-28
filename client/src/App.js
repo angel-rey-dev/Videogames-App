6619938048a4889c757e1da712463e52e0b1037e
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-10-28 08:08:49 -0400

    Add basic styles to landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
-import HomePage from './components/HomePage'
-import LandingPage from './components/LandingPage'
+import './App.scss';
+import HomePage from './components/HomePage/HomePage'
+import LandingPage from './components/LandingPage/LandingPage'
 
 // react-rouder-dom
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>Henry Videogames</h1>
+        <h1 className="sr-only">THE GAMEROOM</h1>
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/home" component={HomePage} />
