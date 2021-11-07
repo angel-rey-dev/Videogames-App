5b463cae96133e76b9d6a606b3dc8a47b08ab180
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-11-06 20:21:21 -0400

    Update landing page styles

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,11 +1,13 @@
+import "./LandingPage.scss"
 import React from 'react'
 import { Link } from "react-router-dom";
-import "./LandingPage.scss"
 import logo from '../../assets/landing-page-logo.svg'
+import SocialMedia from '../SocialMedia/SocialMedia';
 
 export default function LandingPage() {
     return (
         <div className="landing-page">
+            <SocialMedia />
             <div className="overlay">
                 <section className="landing-page__content">
                     <img
