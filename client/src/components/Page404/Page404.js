5c66faf1e6122188ef108bca1058c61df5b5d94b
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-11-08 19:34:39 -0400

    Add 404 page styles

--- client/src/components/Page404/Page404.js
+++ client/src/components/Page404/Page404.js
@@ -1,9 +1,19 @@
+import "./Page404.scss"
 import React from 'react'
+import { Link } from 'react-router-dom'
+import illustration from '../../assets/404/space-404.svg'
+
 
 export default function Page404() {
     return (
-        <div>
-            404
-        </div>
+        <section className="page-404">
+            <div className="page-404__content">
+                <img src={illustration} alt="404" />
+                <h2>Page <span>not</span> found</h2>
+                <Link to="/home" className="page-404__btn">
+                    Go to home page
+                </Link>
+            </div>
+        </section>
     )
 }
