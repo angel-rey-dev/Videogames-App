62514d2c8d5886d63009cb7e297fc37e262d9b90
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-11-09 13:53:28 -0400

    Update

--- client/src/components/FormModal/FormModal.js
+++ client/src/components/FormModal/FormModal.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom"
 export default function FormModal() {
     return (
         <section className="form-modal">
+           
             <div className="form-modal__container">
 
                 <div className="form-modal__content" >
