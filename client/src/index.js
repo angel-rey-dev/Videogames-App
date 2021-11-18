5c3579b8170cb827230044b70d71f967174e6bcd
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-11-17 20:22:40 -0400

    Update

--- client/src/index.js
+++ client/src/index.js
@@ -6,11 +6,11 @@ import reportWebVitals from './reportWebVitals';
 // ------- React Redux
 import { Provider } from 'react-redux'
 import { store } from './redux/store/index';
-import dotenv from "dotenv"
 import axios from 'axios';
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001"
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
